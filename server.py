0ba9c30b042018ff4d8e367656e0160feaa34b0e
Verenich Tatiana <verenik91@gmail.com>
2024-02-13 11:13:33 +0300

    Сети домашнее задание 6, сервер, работа сокетов

--- server.py
+++ server.py
@@ -1,21 +1,30 @@
-#!/bin/python3
+# Необходимо написать программу-клиент, 
+# которая будет цепляться к вашему серверу в Интернете. 
+# Послем того как соединение произошло, 
+# первое сообщение будет ником пользователя, а далее, 
+# любое отправленное сообщение будет транслироваться другим клиентам. 
+# Таким образом пообщаемся между собой.
+
 import socket
 import threading
+import logging
 
-# Connection Data
-host = 'ENTER YOUR IP'
+# Настройка логирования
+logging.basicConfig(filename='server_log.txt', level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+#данные для подключения
+host = '127.0.0.1'
 port = 55555
 
-# Starting Server
+#стартуем сервер
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen()
 
-# Lists For Clients and Their Nicknames
 clients = []
 nicknames = []
 
-# Sending Messages To All Connected Clients
 def broadcast(message):
     for client in clients:
         client.send(message)
@@ -27,37 +36,50 @@ def handle(client):
             # Broadcasting Messages
             message = client.recv(1024)
             broadcast(message)
-        except:
-            # Removing And Closing Clients
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            nickname = nicknames[index]
-            broadcast('{} left!'.format(nickname).encode('ascii'))
-            nicknames.remove(nickname)
-            break
+        except Exception as e:
+                logging.error(f"Error in handle(): {e}")
+                print(f"Error in handle(): {e}")
+                # Removing And Closing Clients
+                index = clients.index(client)
+                clients.remove(client)
+                client.close()
+                nickname = nicknames[index]
+                broadcast('{} left!'.format(nickname).encode('ascii'))
+                nicknames.remove(nickname)
+                break
 
-# Receiving / Listening Function
+# Listening Function
 def receive():
-    while True:
-        # Accept Connection
-        client, address = server.accept()
-        print("Connected with {}".format(str(address)))
+    try:
+        while True:
+            # Accept Connection
+            client, address = server.accept()
+            logging.info(f"Connected with {address}")
 
-        # Request And Store Nickname
-        client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
-        nicknames.append(nickname)
-        clients.append(client)
+            # Request And Store Nickname
+            client.send('NICK'.encode('ascii'))
+            nickname = client.recv(1024).decode('ascii')
+            nicknames.append(nickname)
+            clients.append(client)
 
-        # Print And Broadcast Nickname
-        print("Nickname is {}".format(nickname))
-        broadcast("{} joined!".format(nickname).encode('ascii'))
-        client.send('Connected to server!'.encode('ascii'))
+            # Print And Broadcast Nickname
+            logging.info(f"Nickname is {nickname}")
+            broadcast(f"{nickname} joined!".encode('ascii'))
+            client.send('Connected to server!'.encode('ascii'))
 
-        # Start Handling Thread For Client
-        thread = threading.Thread(target=handle, args=(client,))
-        thread.start()
+            # Start Handling Thread For Client
+            thread = threading.Thread(target=handle, args=(client,))
+            thread.start()
+
+    except KeyboardInterrupt:
+        logging.info("Server stopped by user")
+    finally:
+        for client in clients:
+            try:
+                client.close()  # Закрываем соединения с клиентами
+            except Exception as e:
+                logging.error(f"Error closing client connection: {e}")
+        server.close()
 
 print("Server if listening...")
 receive()
